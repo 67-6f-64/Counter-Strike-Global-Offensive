d14fcade65dfe617344a37a1413deb6b61482746
Júlio César <schinmor@hotmail.com>
2015-07-23 13:58:48 -0300

    offset.h

---
+++ offset.h
@@ -0,0 +1,15 @@
+#ifndef OFFSET_H
+#define OFFSET_H
+
+
+struct Entity
+{
+	DWORD localPlayer = 0xA77D2C;
+	DWORD entityBase = 0x4A19D84;
+	DWORD ihealth = 0xFC;
+	DWORD iTeam = 0xF0;
+	DWORD fFlags = 0x100;
+}entity;
+
+
+#endif
