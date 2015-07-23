caa1f1d004287ccf6e134f8e85d72526c5170243
Júlio César <schinmor@hotmail.com>
2015-07-23 15:59:43 -0300

    Update offset.h

--- offset.h
+++ offset.h
@@ -1,15 +1,27 @@
 #ifndef OFFSET_H
 #define OFFSET_H
 
+#include <Windows.h>
 
+//struct with all offsets provide by Y3t1y3t 'dumper TY!
 struct Entity
 {
+	//localPlayer
 	DWORD localPlayer = 0xA77D2C;
+	DWORD iCrossHairID = 0x2410;
+
+	//entity
 	DWORD entityBase = 0x4A19D84;
+	DWORD loopDistance = 0x10;
+
+	//comuns
+	DWORD index = 0x64;
 	DWORD ihealth = 0xFC;
 	DWORD iTeam = 0xF0;
 	DWORD fFlags = 0x100;
-}entity;
+	DWORD vecOrigin = 0x134;
+};
 
+extern Entity entity;
 
 #endif
