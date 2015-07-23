aabc04b65f3c12dfafe246987464c613c9d05b42
Júlio César <schinmor@hotmail.com>
2015-07-23 18:29:28 -0300

    Update main.h
    add trigger

--- main.h
+++ main.h
@@ -1,77 +1,12 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef MAIN_H_
+#define MAIN_H_
 
-#include <Windows.h>
 #include <iostream>
+#include <Windows.h>
 #include <TlHelp32.h>
-#include "offset.h"
+#include <regex>
+#include <string>
 
-//VECTOR x,y,z struct for vecOrigin
-typedef struct
-{
-	float   x;
-	float   y;
-	float   z;
-}Vector, *PVector;
-
-//Class to get data from game
-class Memory
-{
-public:
-	Memory(); //constructor
-	~Memory(); //desctructor
-
-	bool reading = false;
-	void StartReadMemory();
-	static DWORD ReadMemory(LPVOID lParam);
-
-	DWORD Module(LPCSTR moduleName, DWORD pId); //module address's .exe .dll
-	DWORD GetClientDll(); //client.dll
-	DWORD GetEngineDll(); //engine.dll
-
-	//localPlayer functions
-	DWORD GetlocalPlayer(); //get localPlayer	
-	int GetmyHealth(); //get data health
-	int GetmyTeam(); //get data team
-	int GetmyFlags(); //get data Flags
-	int GetmyCrossId(); // get data crossairId
-	int GetmyId(); // get data index
-	Vector GetmyPos(); //get data pos: x, y, z
-
-	//entityLoop functions	
-	int GetEntHealth(DWORD entAddress); //get data health
-	int GetEntTeam(DWORD entAddress); //get data team
-	int GetEntFlags(DWORD entAddress); //get data Flags
-	int GetEntCrossId(DWORD entAddress); // get data crossairId
-	int GetEntId(DWORD entAddress); // get data index	
-	Vector GetEntPos(DWORD entAddress); //get data pos: x, y, z
-
-protected:
-	HWND gameHwnd;
-	HANDLE proc;
-	DWORD gamePID;
-	DWORD clientDll;
-	DWORD engineDll;
-
-	//localplayer
-	DWORD localPlayerBase;
-	int health;
-	int team;
-	int flags;
-	int crossId;
-	int index;
-	Vector myPos;
-
-	//entity
-	DWORD entityLoop;
-	int ehealth;
-	int eteam;
-	int eflags;
-	int eindex;
-	Vector ePos;
-};
-
-//ReadProcessMemory template
 template<typename TYPE>
 TYPE RPM(HANDLE proc, DWORD address, SIZE_T size){
 	TYPE buffer = TYPE();
@@ -79,12 +14,97 @@ TYPE RPM(HANDLE proc, DWORD address, SIZE_T size){
 	return buffer;
 }
 
-//WriteProcessMemory template
 template<typename TYPE>
 BOOL WPM(HANDLE proc, DWORD address, TYPE dataToWrite){
 	TYPE buffer = dataToWrite;
 	return ::WriteProcessMemory(proc, (LPVOID)address, &buffer, sizeof(buffer), 0);
 }
 
+typedef struct
+{
+	float   x;
+	float   y;
+	float   z;
+}Vector, *PVector;
+
+class cheat{
+public:
+	HWND game;
+	DWORD gamePid;
+	HANDLE gameHandle;
+
+	DWORD GetModule(LPCSTR processName, int processPid);
+	void GetAddress();
+
+	bool aim = false;
+	void startAimbot();
+	static DWORD Aimbot(LPVOID lpParam);
+	double Distance(Vector a, Vector b, bool metters);
+	Vector GetBone(int iBone);
+	Vector BonePos(DWORD target, DWORD address, int boneId);
+	bool WorldToScreen(Vector vStart, Vector &vOut, float *flMatrix);
+	float DistanceBetweenCross(float X, float Y);
+	Vector CalcAngle(Vector& src, Vector& dst);
+
+	bool glow = false;
+	void startGlow();
+	static DWORD Glow(LPVOID lpParam);
+
+	bool radar = false;
+	void startRadar();
+	static DWORD Radar(LPVOID lpParam);
+
+	bool norecoil = false;
+	void startNoRecoil();
+	static DWORD NoRecoil(LPVOID lpParam);
+	bool IsRifle(int id);
+	float NormalX(float angle);
+	float NormalY(float angle);
+
+	bool readmemory = false;
+	void startReadMemory();
+	static DWORD ReadMemory(LPVOID lpParam);
+	void WriteGlow(HANDLE proc, float r, float g, float b, float a, bool Occluded, bool Unoccluded);
+
+	//dlls
+	DWORD clientDll;
+	DWORD engineDll;
+	//pointers
+	DWORD myPlayer;
+	DWORD entityBase;
+	DWORD entity;
+	DWORD engine;
+	DWORD glowBs;
+	DWORD weaponEntity;
+	DWORD weaponBaseIndex;
+	DWORD myPlayerBoneBase;
+
+	//vars
+	int myPlayerHealth;
+	int myPlayerTeam;
+	int entityHealth;
+	int entityTeam;
+	bool entityDormant;
+	int glowIndex;
+	int shotsFired;
+	Vector aPunch;
+	Vector oldAngle;
+	int weaponId;
+	int weaponAmmo;
+	bool weaponReloading;
+	Vector vMatrix;
+};
+
+class others{
+public:
+	int option;
+
+	bool isNumber(int input);
+	void Menu();
+
+};
+
+extern cheat Cheat;
+extern others Others;
 
 #endif
