dce49c9ad072c5e31f721ec1c23982074272b245
Júlio César <schinmor@hotmail.com>
2015-07-23 18:19:35 -0300

    main.cpp
    Added Menu, Trigger bot

--- main.cpp
+++ main.cpp
@@ -3,40 +3,57 @@
 //#include "offset.h" in main.h and extern it here so every cpp file have access to it
 Entity entity;
 
+Memory* mem = new Memory(); //create new object of memory class
+
+int Menu()
+{
+	int option = 0;
+	system("cls");
+	std::cout << "MENU: choose a number and type [ENTER]" << std::endl;
+	std::cout << "(1) Only Read Memory " << mem->reading <<std::endl;
+	std::cout << "(2) TriggerBot inCrossHairId " << mem->trigger << std::endl;
+
+	std::cout << "(0) EXIT" << std::endl;
+	std::cin >> option;
+	switch (option)
+	{
+	case 1:			
+		if (!mem->reading){
+			mem->StartReadMemory();
+			mem->reading = true;
+		}		
+		Menu();	
+		break;
+	case 2:			
+		if (!mem->trigger)
+		{
+			mem->StartTrigger();
+			mem->trigger = true;
+		}							
+		Menu();
+		break;		
+	case 0:
+		return 0;
+		break;
+	default:
+		Menu();
+		break;
+	}
+}
+
 int main()
 {
 	int option = 0;
 	system("Color 1B");
-
-	Memory* mem = new Memory(); //create new object of memory class
 
 	// to get health use mem->GetHealth() e.g
 
 	std::cout << "-----------ADDRESS'S CLIENT.DLL ENGINE.DL----------------------------" << std::endl;
 	std::cout << "client.dll: " << mem->GetClientDll() << " engine.dll: " << mem->GetEngineDll() << std::endl << std::endl;
 
-	std::cout << "Start Thread to Read Memory? (1) to yes, (2) to no" << std::endl;
-	std::cin >> option;
-	
-	
-	switch (option)
-	{
-	case 1:	
-		while (true){
-			if (!mem->reading)
-				mem->StartReadMemory();
-			Sleep(100000);
-		}
-		break;
-	case 2:		
-		delete mem; //delete object
-		return 0;
-		break;
-	default:
-		delete mem; //delete object
-		return 0;
-		break;
-	}
+	Menu();
+
+	delete mem;
 
 	return 0;
 }
