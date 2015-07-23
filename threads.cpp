890317bd5312b7468dbcf85e8947d85f14391ec4
Júlio César <schinmor@hotmail.com>
2015-07-23 15:59:23 -0300

    Update threads.cpp

--- threads.cpp
+++ threads.cpp
@@ -1,22 +1,55 @@
 #include "main.h"
+#include "offset.h"
 
+//since our treadh is a static one inside a class we need to call the class here to access functions
 Memory memory;
 
-void Memory::StartReadMemory()
+void Memory::StartReadMemory() //call this function to start ReadMemory thread
 {
+	memory.reading = true;
 	HANDLE start = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ReadMemory, NULL, 0, NULL);
 }
 
-DWORD Memory::ReadMemory(LPVOID lParam)
+DWORD Memory::ReadMemory(LPVOID lParam) // thread: ReadMemory where we get data from memory's game
 {
 	while (true)
 	{
 		system("cls");
-		std::cout << "My Player->" << std::endl;
-		std::cout << "Health: " << memory.GetmyHealth() << " Team: " << memory.GetmyTeam()
-			<< " Flags: " << memory.GetmyFlags()
-			<< " X: " << memory.GetmyPos().x << " Y: " << memory.GetmyPos().y << " Z: " << memory.GetmyPos().z << std::endl;
-		Sleep(250);
+		/*                      LOCALPLAYER INFO                                       */
+		std::cout << "My Player->[" << memory.GetmyId() << "]" << std::endl << std::endl;
+		std::cout << "-> Health: " << memory.GetmyHealth() << std::endl;
+		std::cout << "-> Team: " << memory.GetmyTeam() << std::endl;
+		std::cout << "-> Flags: " << memory.GetmyFlags() << std::endl;
+		std::cout << "-> Crosshair Id: " << memory.GetmyCrossId() << std::endl;
+		std::cout << "-> localPlayer Position X: " << memory.GetmyPos().x 
+			<< " Y: " << memory.GetmyPos().y 
+			<< " Z: " << memory.GetmyPos().z << std::endl << std::endl;
+		
+		/*                      LIST LOCALPLAYER ENEMYS                                  */
+		for (int i = 0; i < 64; i++)
+		{
+			memory.entityLoop = RPM<DWORD>(memory.proc, ((memory.clientDll + entity.entityBase) + ((i - 1)*entity.loopDistance)), sizeof(DWORD));
+			if (memory.entityLoop == NULL)
+				continue;
+
+			if (memory.GetEntTeam(memory.entityLoop) != memory.GetmyTeam())
+			{
+				std::cout << "Enemy->[" << memory.GetEntId(memory.entityLoop) << "]" << std::endl << std::endl;
+				std::cout << "-> Health: " << memory.GetEntHealth(memory.entityLoop) << std::endl;
+				std::cout << "-> Team: " << memory.GetEntTeam(memory.entityLoop) << std::endl;
+				std::cout << "-> Flags: " << memory.GetEntFlags(memory.entityLoop) << std::endl;
+				std::cout << "-> Enemy's Position X: " << memory.GetEntPos(memory.entityLoop).x 
+					<< " Y: " << memory.GetEntPos(memory.entityLoop).y 
+					<< " Z: " << memory.GetEntPos(memory.entityLoop).z << std::endl << std::endl;
+			}
+
+		}
+
+		
+		if (!memory.reading)
+			break;
+
+		Sleep(5000);
 	}
 	return 0;
 }
