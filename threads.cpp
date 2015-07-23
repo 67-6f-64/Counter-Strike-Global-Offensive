f3a461f63e05572a62da84eafb87848078d123c8
Júlio César <schinmor@hotmail.com>
2015-07-23 18:18:51 -0300

    treads.cpp add trigger
    Added trigger bot

--- threads.cpp
+++ threads.cpp
@@ -1,8 +1,78 @@
 #include "main.h"
 #include "offset.h"
 
-//since our treadh is a static one inside a class we need to call the class here to access functions
+//since our thread is a static one inside a class we need to call the class object here to access functions
 Memory memory;
+
+void Memory::StartTrigger()
+{
+	HANDLE start = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Trigger, NULL, 0, NULL);
+}
+
+//THIS TRIGGER ONLY WORKS AFTER ALL ENEMYS JOIN THE ENEMY TEAM
+//checking team first
+//create a list with all target adress's
+//loop target and check crosshair id and target id
+//shoot
+
+DWORD Memory::Trigger(LPVOID lParam)
+{
+	memory.trigger = true;
+	std::vector<DWORD> enemys;
+	int oldTeam = 0;
+	while (true)
+	{
+		/*
+		a) check if we are in the same team yet.
+		b) if not get new targets else avoid read everything again and use what we have
+		*/
+		if (memory.GetmyTeam() != oldTeam) 
+		{
+			oldTeam = memory.GetmyTeam(); // since we are in a differnt team set the old to actual
+
+			enemys.clear(); //clear to get new enemys address's
+
+			for (int i = 0; i < 64; ++i) //loop to get enemy's
+			{
+				memory.entityLoop = RPM<DWORD>(memory.proc, ((memory.clientDll + entity.entityBase) + ((i - 1)*entity.loopDistance)), sizeof(DWORD));
+				
+				if (memory.entityLoop == NULL) //if entity is not valid
+					continue;				
+
+				if (memory.GetEntTeam(memory.entityLoop) != 0 && memory.eteam != memory.team) //if entity is not 0 and is not our team add to vector
+				{
+					enemys.push_back(memory.entityLoop);
+				}
+			}
+
+		}
+
+		
+		if (memory.GetmyTeam() == oldTeam) //i'm in the same team yet, so i don't need to get all entitys again
+		{			
+			for (std::vector<DWORD>::size_type i = 0; i != enemys.size(); ++i) //loop enemys/targets
+			{				
+				if (memory.GetEntId(enemys[i]) == memory.GetmyCrossId() && memory.GetEntHealth(enemys[i]) > 0) //entity id is equal to our crosshairId?
+				{
+					//shoot: write to the memory +attack / -attack
+					WPM<int>(memory.proc, (memory.clientDll + entity.forceAttack), 5);
+					Sleep(25);
+					WPM<int>(memory.proc, (memory.clientDll + entity.forceAttack), 4);
+					Sleep(70);
+				}
+			}
+		}
+
+		
+
+		if (!memory.trigger)
+			break;
+
+		Sleep(16);
+	}
+
+	return 0;
+}
 
 void Memory::StartReadMemory() //call this function to start ReadMemory thread
 {
