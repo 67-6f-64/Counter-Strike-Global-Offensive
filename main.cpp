cdff303f7e5a923dd183117a7abf0afe98c43a3e
Júlio César <schinmor@hotmail.com>
2015-07-24 11:17:02 -0300

    Update main.cpp
    added no flash

--- main.cpp
+++ main.cpp
@@ -7,13 +7,12 @@ Memory* mem = new Memory(); //create new object of memory class
 
 int Menu()
 {
-	int option = 0;
-	system("cls");
+	int option = 0;	
 	std::cout << "MENU: choose a number and type [ENTER]"						<< std::endl;
 	std::cout << "(1) Only Read Memory "					<< mem->reading		<< std::endl;
 	std::cout << "(2) TriggerBot inCrossHairId "			<< mem->trigger		<< std::endl;
 	std::cout << "(3) Aimbot (No Vish Check yet) "			<< mem->aim			<< std::endl;
-
+	std::cout << "(4) Remove Flash "						<< mem->noflash		<< std::endl;
 	std::cout << "(0) EXIT" << std::endl;
 	std::cin >> option;
 	switch (option)
@@ -22,15 +21,21 @@ int Menu()
 		if (!mem->reading){
 			mem->StartReadMemory();
 			mem->reading = true;
-		}		
-		Menu();	
+		}
+		else
+			mem->reading = false;
+		system("cls");
+		Menu();
 		break;
 	case 2:			
 		if (!mem->trigger)
 		{
 			mem->StartTrigger();
 			mem->trigger = true;
-		}							
+		}
+		else
+			mem->trigger = false;
+		system("cls");
 		Menu();
 		break;
 	case 3:
@@ -38,6 +43,20 @@ int Menu()
 			mem->StartAim();
 			mem->aim = true;
 		}
+		else
+			mem->aim = false;
+		system("cls");
+		Menu();
+		break;
+
+	case 4:
+		if (!mem->noflash){
+			mem->StartNoFlash();
+			mem->noflash = true;
+		}
+		else
+			mem->noflash = false;
+		system("cls");
 		Menu();
 		break;
 	case 0:
@@ -53,8 +72,7 @@ int Menu()
 }
 
 int main()
-{
-	int option = 0;
+{	
 	system("Color 1B");
 
 	// to get health use mem->GetHealth() e.g
